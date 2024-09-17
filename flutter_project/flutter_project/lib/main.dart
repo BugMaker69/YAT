b9ee77b2c5e95638c68e5cc35eb0a4e269a7af3d
Omar Elsaadany <elsaadanythehunter@gmail.com>
2024-09-17 14:20:41 +0300

    Edit Themes

--- flutter_project/flutter_project/lib/main.dart
+++ flutter_project/flutter_project/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project/data/models/card_data.dart';
 import 'package:flutter_project/main_page.dart';
+import 'package:flutter_project/themes/theme.dart';
 import 'package:hive_flutter/adapters.dart';
 
 void main() async {
@@ -18,10 +19,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'ToDo App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: ligthMdoe,
+      darkTheme: darkMode,
       home: MainScreen(),
     );
   }
