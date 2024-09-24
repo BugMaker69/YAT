f1532b7b822a4df6fb86334cd5ad4804e0660ccb
Omar Elsaadany <elsaadanythehunter@gmail.com>
2024-09-24 16:07:34 +0300

    BlocBuilder , Refactor Code

--- flutter_project/flutter_project/lib/main.dart
+++ flutter_project/flutter_project/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_project/cubits/todo_cubit.dart';
 import 'package:flutter_project/data/models/card_data.dart';
 import 'package:flutter_project/pages/main_page.dart';
 import 'package:flutter_project/pages/settings_page.dart';
@@ -10,27 +12,32 @@ import 'package:provider/provider.dart';
 void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(CardDataAdapter());
-  await Hive.openBox<CardData>('todoBox');
+  final todoBox = await Hive.openBox<CardData>('todoBox');
   runApp(
     ChangeNotifierProvider(
       create: (context) => ThemeProvider(),
-      child: MyApp(),
+      child: MyApp(todoBox: todoBox),
     ),
   );
   // runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final Box<CardData> todoBox;
+  const MyApp({required this.todoBox});
+  // const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'ToDo App',
-      theme: ligthMdoe,
+      theme: Provider.of<ThemeProvider>(context).currentTheme,
       darkTheme: darkMode,
-      home: MainScreen(),
+      home: BlocProvider(
+        create: (context) => TodoCubit(todoBox),
+        child: MainScreen(),
+      ),
       routes: {'/settingspage': (context) => SettingsScreen()},
     );
   }
--- flutter_project/flutter_project/lib/pages/main_page.dart
+++ flutter_project/flutter_project/lib/pages/main_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_project/add_item.dart';
 import 'package:flutter_project/card_item.dart';
-import 'package:flutter_project/data/database.dart';
+import 'package:flutter_project/cubits/todo_cubit.dart';
 import 'package:flutter_project/data/models/card_data.dart';
-import 'package:hive_flutter/adapters.dart';
 
 class MainScreen extends StatefulWidget {
   MainScreen({super.key});
@@ -13,41 +13,10 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  final _myBox = Hive.box<CardData>('todoBox');
-  var todoDataBase = TodoDataBase();
-
-  List<CardData> todoList = [];
-
-  @override
-  void initState() {
-    // if (_myBox.values.isNotEmpty) {
-    if (_myBox.length > 0) {
-      todoList = todoDataBase.loadTodos();
-    }
-  }
-
-  void onCheckboxChanged(bool? value, int index) {
-    setState(() {
-      todoList[index] =
-          CardData(isChecked: !value!, data: todoList[index].data);
-      // _myBox.putAt(index, todoList[index]);
-
-      todoDataBase.updateTodo(index, todoList[index]);
-
-      // todoDataBase.deleteTodo(index);
-      // todoDataBase.addTodo(todoList[index]);
-    });
-  }
-
   final _controller = TextEditingController();
 
   void saveNewTask() {
-    setState(() {
-      CardData newTask = CardData(isChecked: false, data: _controller.text);
-      todoList.add(newTask);
-      todoDataBase.addTodo(newTask);
-    });
-
+    context.read<TodoCubit>().addTodo(_controller.text);
     _controller.clear();
     Navigator.pop(context);
   }
@@ -78,8 +47,8 @@ class _MainScreenState extends State<MainScreen> {
         actions: [
           IconButton(
             onPressed: () {
-                  Navigator.pushNamed(context, '/settingspage');
-                },
+              Navigator.pushNamed(context, '/settingspage');
+            },
             icon: Container(
               decoration: BoxDecoration(
                 color: Colors.white.withOpacity(.1),
@@ -100,25 +69,26 @@ class _MainScreenState extends State<MainScreen> {
         onPressed: createNewTask,
         child: Icon(Icons.add),
       ),
-      body: ListView.separated(
-        itemBuilder: (context, index) => Dismissible(
-          key: Key(todoList[index].data),
-          direction: DismissDirection.endToStart,
-          onDismissed: (direction) {
-            setState(() {
-              todoList.removeAt(index);
-              todoDataBase.deleteTodo(index);
-            });
-          },
-          child: CardItem(
-            data: todoList[index].data,
-            isChecked: todoList[index].isChecked,
-            onChanged: (value) =>
-                onCheckboxChanged(todoList[index].isChecked, index),
-          ),
-        ),
-        itemCount: todoList.length,
-        separatorBuilder: (context, index) => SizedBox(height: 16),
+      body: BlocBuilder<TodoCubit, List<CardData>>(
+        builder: (context, todoList) {
+          return ListView.separated(
+            itemBuilder: (context, index) => Dismissible(
+              key: Key(todoList[index].data),
+              direction: DismissDirection.endToStart,
+              onDismissed: (direction) {
+                context.read<TodoCubit>().deleteTodo(index);
+              },
+              child: CardItem(
+                data: todoList[index].data,
+                isChecked: todoList[index].isChecked,
+                onChanged: (value) =>
+                    context.read<TodoCubit>().updateTodo(index),
+              ),
+            ),
+            itemCount: todoList.length,
+            separatorBuilder: (context, index) => SizedBox(height: 16),
+          );
+        },
       ),
     );
   }
