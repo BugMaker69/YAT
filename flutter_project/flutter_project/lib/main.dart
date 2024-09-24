23a26888135fc72836b5ad383bbb024051d61472
Omar Elsaadany <elsaadanythehunter@gmail.com>
2024-09-24 15:10:58 +0300

    add ChangeNotifierProvider

--- flutter_project/flutter_project/lib/main.dart
+++ flutter_project/flutter_project/lib/main.dart
@@ -2,14 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_project/data/models/card_data.dart';
 import 'package:flutter_project/pages/main_page.dart';
 import 'package:flutter_project/pages/settings_page.dart';
+import 'package:flutter_project/themes/ThemeProvider.dart';
 import 'package:flutter_project/themes/theme.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(CardDataAdapter());
   await Hive.openBox<CardData>('todoBox');
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: MyApp(),
+    ),
+  );
   // runApp(const MyApp());
 }
 
@@ -24,9 +31,7 @@ class MyApp extends StatelessWidget {
       theme: ligthMdoe,
       darkTheme: darkMode,
       home: MainScreen(),
-      routes: {
-        '/settingspage': (context) => SettingsScreen()
-      },
+      routes: {'/settingspage': (context) => SettingsScreen()},
     );
   }
 }
